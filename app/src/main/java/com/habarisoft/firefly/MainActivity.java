3aa01d3435660b73ee5af0ddbeb6f6f1025318db
Michael Justin <info@habarisoft.com>
2019-03-17 11:16:04 +0100

    no message

--- app/src/main/java/com/habarisoft/firefly/MainActivity.java
+++ app/src/main/java/com/habarisoft/firefly/MainActivity.java
@@ -8,30 +8,37 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.CellInfo;
 import android.telephony.CellInfoLte;
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TelephonyManager tManager;
+    TelephonyManager telephonyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkLte();
+        telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        telephonyManager.listen(new PhoneStateListener() {
+            @Override
+            public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+                printLteSignalStrengths();
+            }
+        }, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
     }
 
-    private void checkLte() {
-        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-
+    private void printLteSignalStrengths() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
 
-        List<CellInfo> cellInfoList = tm.getAllCellInfo();
+        List<CellInfo> cellInfoList = telephonyManager.getAllCellInfo();
         for (CellInfo cellInfo : cellInfoList) {
             if (cellInfo instanceof CellInfoLte) {
                 // cast to CellInfoLte and call all the CellInfoLte methods you need
